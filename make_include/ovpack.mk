08f119fa29ef1ab2857cd48b1a4d23c4ec89e597
Ben <bjmorse@gmail.com>
2021-10-21 20:02:36 -0700

    better working

--- make_include/ovpack.mk
+++ make_include/ovpack.mk
@@ -3,21 +3,27 @@ ifdef f
 
 create:
 	@echo file: ${f}
+	@echo "\n"
+
 
 indent:
 	sed -i 's/^/,/' ${f}
 
+
+
+
 ifdef a
 
-# append-git:
-# 	@echo ${h} ${}
+# using https://stackoverflow.com/questions/40477948/prepend-a-text-to-a-file-in-makefile to append
+# to head of line
+# however it doesnt work if the file is empty
+# so all of these are copied
 
 append-submodule:
-	touch ${f}
-	@echo submodule, ${a}, ${b} >> ${f}
+	test -f ${f} && sed -i "1i submodule, ${a}, ${b}" ${f} || echo submodule, ${a}, ${b} > ${f}
+# 	sed -i "" '1{h;s/.*/${a}/;G;}' input
 append-git:
-	touch ${f}
-	sed -i "1i git, ${a}, ${b}" ${f}
+	test -f ${f} && sed -i "1i git, ${a}, ${b}" ${f} || echo git, ${a}, ${b} > ${f}
 	# @echo git, ${a}, ${b} >> ${f}
 
 # echo "$(echo -n 'git, ${a}, ${b}'; cat ${f})" > new.txt

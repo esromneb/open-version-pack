94c719984f67496a116373e17496a652a5618885
Ben <bjmorse@gmail.com>
2021-10-21 21:03:31 -0700

    About to switch

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
 const parse = require('csv-parse');
 const assert = require('assert');
+const jsonToDot = require('json-to-dot');
 
 
 const path = process.argv[2];
 
 
 
-let myparse = (input) => {
+let myparse = (input, cb) => {
   console.log('about to parse' + input);
   parse(input, {
     comment: '#',
@@ -15,16 +16,55 @@ let myparse = (input) => {
     // columns: ['a','b','c','d','g'],
     // skip_lines_with_error: true,
 
-  }, function(err, output){
-    console.log(output);
-    console.log(err);
+  }, function(err, output) {
+    cb(output);
+    // console.log(output);
+    // console.log(err);
     // assert.deepStrictEqual(
     //   output,
     //   [ [ '1', '2', '3', '4' ], [ 'a', 'b', 'c', 'd' ] ]
     // )
   })
-
 }
+
+
+let tojson = (o) => {
+  let r = {};
+  let level = [];
+  // loop through and sort by indentation
+  for(row of o) {
+    const rl = row.length;
+    // console.log(`${row} foo ${rl}`);
+    if( typeof level[rl] === 'undefined' ) {
+      // level[rl].push(row);
+      level[rl] = []
+    }
+    level[rl].push(row);
+    // console.log(`${rl}`);
+  }
+
+  // removes high levels equal to the number of default rows
+  let check = 0;
+  while(1) {
+    if( typeof level[check] === 'undefined' ) {
+      level.shift();
+    } else {
+      break;
+    }
+  }
+
+  
+
+  for(let i = 0; i < level.length; i++) {
+    
+  }
+
+
+
+  console.log(level);
+  return level;
+}
+
 
 
 
@@ -32,17 +72,23 @@ fs.readFile(path, 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
-  console.log(data);
-  myparse(data);
+  // console.log(data);
+  myparse(data,(c)=>{
+    const j = tojson(c);
+    const dot = jsonToDot(j);
+    console.log(dot);
+  });
 });
 
 
 
+const dot = jsonToDot({
+  foo: ['bar', 'buzz'],
+  bar: ['foo'],
+  norf: ['worble', 'buzz'],
+  worf: ['worble'],
+  fizz: ['buzz']   //  fizz->buzz
+});
 
-// const input = '#Welcome\n"1","2","3","4"\n"a","b","c","d"'
 
-
-// const name = 'foo';
-
-
-// console.log(name);
+// console.log(dot);
